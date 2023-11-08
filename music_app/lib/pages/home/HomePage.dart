33e5ca12d31c8667c49a2fa357ce6b8c1ec03290
huyhieu127 <dev.huyhieu@gmail.com>
2023-11-09 01:40:06 +0700

    Add ProfilePage.dart

--- music_app/lib/pages/home/HomePage.dart
+++ music_app/lib/pages/home/HomePage.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:music_app/components/GetPremium.dart';
 import 'package:music_app/components/SongItem1.dart';
 import 'package:music_app/components/SongItem2.dart';
 import 'package:music_app/helper/AppColor.dart';
-import 'package:music_app/helper/AppDecoration.dart';
 import 'package:music_app/helper/AppResource.dart';
 import 'package:music_app/helper/AppRoute.dart';
-import 'package:music_app/pages/library/LibraryPage.dart';
 import 'package:music_app/widgets/AppInkWell.dart';
 import 'package:music_app/widgets/AppTextTopic.dart';
 
@@ -114,70 +113,11 @@ class _HomePageState extends State<HomePage> {
                 child: Column(
                   children: [
                     const SizedBox(height: 8),
-                    // Banner
+                    // Get Premium
                     const SizedBox(height: 8),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                      child: Container(
-                        decoration: AppDecoration.boxDecorationLinearGradient(radius: 24),
-                        child: Stack(
-                          children: [
-                            Positioned(
-                              right: 1,
-                              bottom: 1,
-                              child: Image.asset(
-                                "$assetImage/person_1.png",
-                                height: 150,
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(right: 100.0, left: 25.0, top: 20.0, bottom: 20.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  const Text(
-                                    "Enjoy All Benefits!",
-                                    style: TextStyle(
-                                      color: AppColor.icon2,
-                                      fontSize: 22,
-                                      fontWeight: FontWeight.w700,
-                                    ),
-                                  ),
-                                  const SizedBox(height: 8),
-                                  const Text(
-                                    "Enjoy listening podcast with better audio quality, without restrictions and without ads",
-                                    style: TextStyle(
-                                        color: AppColor.icon2,
-                                        fontSize: 12,
-                                        fontWeight: FontWeight.w400,
-                                        fontStyle: FontStyle.italic),
-                                  ),
-                                  const SizedBox(height: 14),
-                                  AppInkWell(
-                                    onTap: () {},
-                                    radius: 20,
-                                    decoration: AppDecoration.boxDecoration(
-                                      radius: 20,
-                                      color: AppColor.button2,
-                                    ),
-                                    child: const Padding(
-                                      padding: EdgeInsets.symmetric(horizontal: 14, vertical: 6),
-                                      child: Text(
-                                        "Get Premium",
-                                        style: TextStyle(
-                                          color: AppColor.primary,
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.w600,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 24.0),
+                      child: GetPremium(),
                     ),
                     // Subscriptions
                     const SizedBox(height: 16),
--- music_app/lib/pages/profile/ProfilePage.dart
+++ music_app/lib/pages/profile/ProfilePage.dart
@@ -1,4 +1,10 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:music_app/components/GetPremium.dart';
+import 'package:music_app/helper/AppColor.dart';
+import 'package:music_app/helper/AppDecoration.dart';
+import 'package:music_app/helper/AppResource.dart';
+import 'package:music_app/widgets/AppHeader.dart';
+import 'package:music_app/widgets/AppInkWell.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -8,8 +14,165 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final settings = {
+    "edit_profile": "Edit profile",
+    "notification": "Notification",
+    "downloads": "Downloads",
+    "content_settings": "Content Settings",
+    "security": "Security",
+    "language": "Language",
+    "dark_mode": "Dark Mode",
+    "help_center": "Help Center",
+  };
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            // Header
+            AppHeader(
+              label: "My Library",
+              iconButton: "$assetIcon/ic_menu.svg",
+              onTapButton: () {},
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 24.0, top: 4, bottom: 8, right: 12),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Stack(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(35.0),
+                                child: Image.asset(
+                                  "$assetImage/avatar_female_small.jpg",
+                                  width: 70,
+                                  height: 70,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              Positioned(
+                                right: 1,
+                                bottom: 1,
+                                child: Container(
+                                  width: 16,
+                                  height: 16,
+                                  decoration: AppDecoration.boxDecoration(radius: 4),
+                                  child: const Icon(
+                                    Icons.edit,
+                                    color: AppColor.icon2,
+                                    size: 12,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const Expanded(
+                            flex: 1,
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 16.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Taylor Swift",
+                                    style: TextStyle(
+                                      color: AppColor.text1,
+                                      fontWeight: FontWeight.w700,
+                                      height: 1.1,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  SizedBox(height: 8),
+                                  Text(
+                                    "taylor_swift@gmail.com",
+                                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 24.0),
+                      child: GetPremium(),
+                    ),
+                    ListView.separated(
+                      physics: const NeverScrollableScrollPhysics(),
+                      padding: const EdgeInsets.all(12),
+                      itemBuilder: (context, index) {
+                        return AppInkWell(
+                          onTap: () {},
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+                            child: Row(
+                              children: [
+                                const Padding(
+                                  padding: EdgeInsets.only(bottom: 6),
+                                  child: Icon(
+                                    Icons.settings_suggest_rounded,
+                                    size: 28,
+                                  ),
+                                ),
+                                const SizedBox(width: 12),
+                                Expanded(
+                                  child: Text(
+                                    settings.values.elementAt(index),
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ),
+                                Visibility(
+                                  visible: settings.keys.elementAt(index) == "dark_mode",
+                                  child: Switch(
+                                    value: false,
+                                    onChanged: (isSelected) {},
+                                  ),
+                                ),
+                                Visibility(
+                                  visible: settings.keys.elementAt(index) == "language",
+                                  child: const Padding(
+                                    padding: EdgeInsets.only(right: 6),
+                                    child: Text(
+                                      "EN",
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                const Icon(
+                                  Icons.arrow_forward_ios_rounded,
+                                  size: 20,
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                      separatorBuilder: (context, index) => const SizedBox(),
+                      itemCount: settings.length,
+                      shrinkWrap: true,
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
